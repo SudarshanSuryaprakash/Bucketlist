432455befd76e1d90ecb43a3967af75c1149aa95
Sudarshan Suryaprakash <sudarshan16rms@gmail.com>
2022-03-08 08:59:51 +0000

    fix(styling): height and br of box change

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ const Home: NextPage = () => {
   }, []);
   return (
     <div className="bg-gradient-to-r from-amber-400 to-amber-100 flex h-screen bg-blue-200 justify-center items-center">
-      <div className="bg-gradient-to-l from-lime-100 to-white h-1/3 w-6/12 flex flex-col justify-center items-center rounded-lg">
+      <div className="bg-gradient-to-l from-lime-100 to-white h-1/6 lg:h-1/3 w-6/12 flex flex-col justify-center items-center rounded-xl ">
         <h1 className="font-mono text-3xl md:text-5xl lg:text-7xl text-amber-400">COMING</h1>
         <h1 className="font-mono text-3xl md:text-5xl lg:text-7xl">SOON</h1>
       </div>
